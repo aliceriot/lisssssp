6da1a92860d761d435b2a22ad8132ee9cf2747cf
Alice Pote <alice.writes.wrongs@gmail.com>
2017-05-23 16:33:10 -0400

    gonna try the ava test runner for fun

---
+++ lisp_test.js
@@ -0,0 +1,11 @@
+const { lexer } = require("./lisp")
+
+describe('li5p test suite', () => {
+  describe('lexer', () => {
+    it('should tokenize right parens', () => {
+      
+    });
+    
+  });
+  
+});
